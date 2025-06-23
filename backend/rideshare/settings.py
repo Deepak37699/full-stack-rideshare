615691151e6c39dcd70a55742df6bc56204980e2
Deepak Yadav <deepak.yadav37699k@gmail.com>
2025-06-23 20:01:52 +0545

    fix: Update ALLOWED_HOSTS to include additional IP addresses for production

--- backend/rideshare/settings.py
+++ backend/rideshare/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-cfu79*ufq826lr@2^3x7n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,0.0.0.0,10.0.2.2', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition
